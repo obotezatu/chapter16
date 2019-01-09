2a77c323f46192225fcb02e62be3095add1a2a3f
Oleg Botezatu <olbot@inbox.ru>
2019-01-09 20:15:57 +0200

    Add Diplicate and Letter count

--- src/main/java/chapter16/EliminateDuplicate.java
+++ src/main/java/chapter16/EliminateDuplicate.java
@@ -1,10 +1,31 @@
 package chapter16;
 
+import java.util.Scanner;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class EliminateDuplicate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String text = "";
+		Set<String> setOfNames = new HashSet<>();
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (!text.equals("exit")) {
+				System.out.println("Input name: ");
+				text = scanner.nextLine();
+				if (!text.equals("exit")) {
+					setOfNames.add(text);
+				}
+			}
+		}
+		System.out.println("Without duplicate: ");
+		for (String value : setOfNames) {
+			System.out.printf("%s ", value);
+		}
 	}
-
 }
